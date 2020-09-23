b1c1a21f61f77e1ba9c693fd1d8fd46c2a99727a
AALych <71018792+AALych@users.noreply.github.com>
2020-09-23 22:09:26 +0300

    Delete t_ex06.py

--- t_ex06.py
+++
@@ -1,9 +0,0 @@
-import turtle
-turtle.shape('triangle')
-print('Введите количество ног у паука')
-n = int(input())
-for i in range(0, n):
-    turtle.forward(60)
-    turtle.stamp()
-    turtle.backward(60)
-    turtle.left(360 / n)
